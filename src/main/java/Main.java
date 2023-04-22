4b08c38c604b30d019417d778e2b850e82f5e68f
54wgvyy8gymw4qfium <54wgvyy8gymw4qfium@gmail.com>
2023-04-21 18:46:59 -0700

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Please await instructions
+ * Once shared, please update this Java doc with provided problem description
+ **/
+public class Main {
+
+    public ____________ myMethod(String[] strings) {
+
+    }
+
+}
